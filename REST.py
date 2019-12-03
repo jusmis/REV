4cdb970c9e75626d008df941437aab0f3098ccc2
jusmis <44554496+jusmis@users.noreply.github.com>
2019-12-03 18:01:42 +0100

    Add files via upload

--- REST.py
+++ REST.py
@@ -12,7 +12,31 @@ class ReverseString(Resource):
         args = parser.parse_args()
         return args['string'][::-1]
 
+class CheckString(Resource):
+    def post(self):
+        parser.add_argument('string', type=str)
+        args = parser.parse_args()
+        return {
+            'hasUpper': hasUpper(args['string']),
+            'hasLower': hasLower(args['string']),
+            'hasNumbers': hasNumbers(args['string']),
+            'hasSpecial': hasSpecial(args['string'])
+            }
+
+def hasUpper(inputString):
+    return any(char.isupper() for char in inputString)
+
+def hasLower(inputString):
+    return any(char.islower() for char in inputString)
+
+def hasNumbers(inputString):
+    return any(char.isdigit() for char in inputString)
+
+def hasSpecial(inputString):
+    return any(not char.isalnum() for char in inputString)
+
 api.add_resource(ReverseString, '/reverse')
+api.add_resource(CheckString, '/check')
 
 if __name__ == '__main__':
      app.run(port='5002')
